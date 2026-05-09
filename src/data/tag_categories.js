3f2162b37a9f264905d5272f089bb3fd07ebfe50
Zihan Yan <azusayann@gmail.com>
2026-05-09 22:42:53 +0100

    Update tag_categories.js

--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,5 +1,14 @@
-const userDefinedTagCategories = {
-	"type": {
-		"description": "type of the paper"
-	}
-}
+define({
+  "classification": {
+    "color": "#4e79a7",
+    "description": "Sub-section of the literature review"
+  },
+  "evaluation": {
+    "color": "#f28e2c",
+    "description": "Type of evaluation used"
+  },
+  "year": {
+    "color": "#59a14f",
+    "description": "Publication year"
+  }
+});
